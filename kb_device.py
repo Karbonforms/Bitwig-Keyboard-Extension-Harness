9505f9e65ee030b87a25cb00bbfaf21b91090992
KLF <karbonforms@gmail.com>
2018-12-10 19:52:30 +0000

    wee tidy

--- kb_device.py
+++ kb_device.py
@@ -1,8 +1,7 @@
 import mido
 import keyboard
 
-# keyboard.remove_all_hotkeys()
-
+#these are arbitrary
 ID = [0x1, 0x2, 0x9, 0x5]
 SENDID = ID + [0x9, 0x4]
 
@@ -15,15 +14,6 @@ def sendmidi(data_):
     msg = mido.Message('sysex', data=data_)
     outport.send(msg)
     print (msg)
-
-# # 7f7f0604
-# k1 = keyboard.add_hotkey('right', lambda: sendmidi( [0x7f, 0x7f, 0x06, 0x04] ) )
-
-# # 7f7f0605
-# k2 = keyboard.add_hotkey('left', lambda: sendmidi( [0x7f, 0x7f, 0x06, 0x05] ) )
-
-# # send toggle off
-# k3 = keyboard.add_hotkey('ctrl+shift+1', lambda: sendmidi( [0x1, 0x2, 0x3] ) )
 
 k1 = keyboard.add_hotkey('right', lambda: sendmidi( MMC + [0x04] ) ) # 7f7f0604
 k2 = keyboard.add_hotkey('left', lambda: sendmidi( MMC + [0x05] ) ) # 7f7f0605
